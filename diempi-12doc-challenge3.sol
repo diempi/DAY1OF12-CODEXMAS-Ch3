504954680fb4111827410f6619627a7de91db4c1
Didier A. E. Gabiam <1102885+diempi@users.noreply.github.com>
2023-12-15 11:43:26 +0100

    Add files via upload

---
+++ diempi-12doc-challenge3.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ChristmasSum {
+
+    // Function to add two numbers
+    function addNumbers(uint256 num1, uint256 num2) public pure returns (uint256) {
+        return num1 + num2;
+    }
+}
